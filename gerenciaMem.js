83ffb50c3bdcfd826f800dd0c163bb44a6d8df00
RenissonSilva <renissonx@gmail.com>
2019-07-07 17:13:50 -0300

    Adição dos processos B e C

--- gerenciaMem.js
+++ gerenciaMem.js
@@ -2,20 +2,24 @@ readline = require('readline');
 
 rl = readline.createInterface(process.stdin, process.stdout);
 
-memoria=[0,0,0,0];
+memoria=[0,0,0,0,0,0];
 
 function recursiva(){
 	console.log(memoria);
-	rl.question("Qual processo deseja executar (entre A e B), digite 0 para sair: ",function(resposta){
+	rl.question("Qual processo deseja executar (entre A e C), digite 0 para sair: ",function(resposta){
+		if(resposta==0){
+			process.exit();
+		}
+
 		rl.setPrompt(`Deseja colocar em que endereço? `);
 		rl.prompt();
 		rl.on('line',function(respostaEnd){
 			if(resposta == "A"){
 				base=0;
-				limite=2;
+				limite=1;
 
-				if(respostaEnd>=base && respostaEnd<limite){
-					memoria[respostaEnd]=1;
+				if(respostaEnd>=base && respostaEnd<=limite){
+					memoria[respostaEnd]="A";
 					console.clear();
 					recursiva();
 				}else{
@@ -24,22 +28,40 @@ function recursiva(){
 					recursiva();
 				}
 			}
+			else if(resposta == "B"){
+				base=2;
+				limite=3;
+				if(respostaEnd>=base && respostaEnd<=limite){
+					memoria[respostaEnd]="B";
+					console.clear();
+					recursiva();
+				}else{
+					console.clear();
+					console.log("Endereço inválido");
+					recursiva();
+				}
+			}
+
+			else if(resposta == "C"){
+				base=4;
+				limite=5;
+				if(respostaEnd>=base && respostaEnd<=limite){
+					memoria[respostaEnd]="C";
+					console.clear();
+					recursiva();
+				}else{
+					console.clear();
+					console.log("Endereço inválido");
+					recursiva();
+				}
+			}
+
+			else{
+				console.log("Digite um processo válido");
+				recursiva();
+			}
 		})
 
-			// else if(resposta == "B"){
-			// 	console.log("Processo B escolhido");
-			// 	recursiva();
-			// }
-
-			// else if(resposta == "0"){
-			// 	console.log("Inté o/");
-			// 	process.exit();	
-			// }
-
-			// else{
-			// 	console.log("Digite um processo válido");
-			// 	recursiva();
-			// }
 		})
 }
 
