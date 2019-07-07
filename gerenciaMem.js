09ec900282517727a117e5cf1327593b2181e518
RenissonSilva <renissonx@gmail.com>
2019-07-07 20:51:41 -0300

    Associação com txt

--- gerenciaMem.js
+++ gerenciaMem.js
@@ -1,8 +1,17 @@
-readline = require('readline');
+console.clear();
 
+readline = require('readline');
+fs = require('fs');
 rl = readline.createInterface(process.stdin, process.stdout);
 
-memoria=[0,0,0,0,0,0];
+var lerArquivo = fs.createReadStream('processos.txt', {encoding: 'ascii'});
+lerArquivo.on("data", function(data){
+	baseA = parseInt(data[0]-1);
+	baseB = parseInt(data[2]);
+	baseC = parseInt(data[4]);
+});
+
+memoria=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 function impressaoLista(array){
 	console.log();
@@ -23,6 +32,16 @@ function recursiva(){
 	rl.question("Qual processo deseja executar (entre A e C), digite 0 para sair: ",function(resposta){
 		if(resposta==0){
 			process.exit();
+		}else if(resposta=="a" || resposta=="A"){
+			console.log(" Base:0 | Limite: " , baseA);
+		}else if(resposta=="b" || resposta=="B"){
+			console.log(" Base:" , baseA+1 , " | Limite: ", baseA+baseB);
+		}else if(resposta=="c" || resposta=="C"){
+			console.log(" Base:", baseA+baseB+1," | Limite: ", baseA+baseB+baseC)
+		}else{
+			console.clear();
+			console.log("Digite um processo válido.");
+			recursiva();
 		}
 
 		rl.setPrompt(`Deseja colocar em que endereço? `);
@@ -30,7 +49,7 @@ function recursiva(){
 		rl.on('line',function(respostaEnd){
 			if(resposta == "A" || resposta == "a"){
 				base=0;
-				limite=1;
+				limite=baseA;
 
 				if(respostaEnd>=base && respostaEnd<=limite){
 					memoria[respostaEnd]="A";
@@ -43,8 +62,8 @@ function recursiva(){
 				}
 			}
 			else if(resposta == "B" || resposta == "b"){
-				base=2;
-				limite=3;
+				base=baseA+1;
+				limite=baseA+baseB;
 				if(respostaEnd>=base && respostaEnd<=limite){
 					memoria[respostaEnd]="B";
 					console.clear();
@@ -57,8 +76,8 @@ function recursiva(){
 			}
 
 			else if(resposta == "C" || resposta == "c"){
-				base=4;
-				limite=5;
+				base=baseA+baseB+1;
+				limite=baseA+baseB+baseC;
 				if(respostaEnd>=base && respostaEnd<=limite){
 					memoria[respostaEnd]="C";
 					console.clear();
@@ -69,7 +88,6 @@ function recursiva(){
 					recursiva();
 				}
 			}
-
 			else{
 				console.clear();
 				console.log("Digite um processo válido");
